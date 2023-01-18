4788dc071c99a12e34f23710489122465f47a503
happeecat <emma.rees.greville@gmail.com>
2023-01-17 18:08:35 -0800

    Added mobs/mob spawner

--- Main.gd
+++ Main.gd
@@ -1,18 +1,17 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-export(PackedScene) var enemy_scene
+export(PackedScene) var mob_scene
 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
 
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.unit_offset = randf()
+	var player_position = $Player.transform.origin
+	mob.initialize(mob_spawn_location.translation, player_position)
+
+	add_child(mob)
