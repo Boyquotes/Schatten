8ad2ab292706c92b9db058338da0e9628dc99705
Bailey <61953092+Garageman2@users.noreply.github.com>
2023-03-01 08:03:51 -0800

    Movement
    Less Broken but still needs work

--- art/Boss/BearAnimElements/BearComposite.gd
+++ art/Boss/BearAnimElements/BearComposite.gd
@@ -18,13 +18,8 @@ func _ready():
 func swing():
 	boss_states.travel("Swing");
 
-#func _physics_process(delta):
-#	var player_loc = player.get("translation");
-#	look_at(player_loc, Vector3.UP)
-#	move_and_slide(Vector3(1,0,0),to_local(Vector3.UP),false,4,0.785398,false);
-#	print(get("translation"));
-	
 func _physics_process(delta):
-	move_and_slide(Vector3(10,0,10) * 1000);
-	if self.get("position.y") != 1.6:
-		self.set("position.y",1.6)
+	var player_loc = player.get("translation");
+	look_at(player_loc, Vector3.UP)
+	move_and_collide(Vector3(-.01,0,0),false)
+
