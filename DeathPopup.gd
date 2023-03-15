f5cc56645bd3ec21bd534d4d78926aa1b5ebaee2
Legendword <zwh@legendword.com>
2023-03-14 19:06:13 -0700

    Don't show pause menu when pressing esc while dead

--- DeathPopup.gd
+++ DeathPopup.gd
@@ -4,8 +4,8 @@ onready var Tw = $Tween;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _deathPopup():
-	popup()
 	get_tree().paused = true
+	show()
 
 func _on_DeathPopup_about_to_show():
 	Tw.interpolate_property(self, "modulate:a", 0.0, 1.0, 0.7, Tween.TRANS_LINEAR, Tween.EASE_IN)
--- PauseMenu.gd
+++ PauseMenu.gd
@@ -4,6 +4,8 @@ var is_shown = false
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
+		if !is_shown and get_tree().paused:
+			return
 		is_shown = !is_shown
 		if !is_shown:
 			hide()
